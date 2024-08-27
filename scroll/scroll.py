f3febd49182a4f0d6ef43f276c538df5baeb6aa6
jmikk <buldog.buldog26@gmail.com>
2024-08-27 13:45:07 -0500

    Update scroll.py

--- scroll/scroll.py
+++ scroll/scroll.py
@@ -1149,9 +1149,9 @@ class Scroll(commands.Cog):
 		tgqueue = root.find("TGQUEUE")
 		await ctx.send(queuereq.text)
 		await ctx.send(root)
-		manual = root.find("manual").text
-		mass = root.find("mass").text
-		api = root.find("api").text
+		manual = tgqueue.find("manual").text
+		mass = tgqueue.find("mass").text
+		api = tgqueue.find("api").text
 		sendstring=f"Current TG Queue:\n```Manual: {manual}\nMass: {mass}\nAPI: {api}```"
 		await ctx.send(sendstring)
 	@commands.command()
